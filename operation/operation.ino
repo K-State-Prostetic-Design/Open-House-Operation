306e8b14fcb0ffb3271148e2b69709d94aeace07
Drake Morgan <drakemorgan@ksu.edu>
2026-02-16 18:48:44 -0600

    Create operation.ino
    init code

---
+++ operation/operation.ino
@@ -0,0 +1,61 @@
+// Have power going to the hand, should be off when not in use
+#define HAND A2
+// The Speaker for you lose, scoring, and welcome
+#define SPEAKER A3
+// Show the Stop playing signal
+#define LED D2
+// connects hand to ground
+#define BOARD D3
+
+/*
+ * The State of the game
+ */
+enum GameState {
+    /*
+     * Nothing Happening
+     */
+    Idle,
+    /*
+     * Prepare game for play
+     */
+    Setup,
+    /*
+     * User is playing the game
+     */
+    Playing,
+    /*
+     * Player loses
+     */
+    Lose,
+}
+
+/*
+ * Universal state of program
+ */
+struct State {
+  GameState state;
+} operation;
+
+/*
+ * Set up the program to know what pins are used for
+ */
+void setup() {
+  PinMode(HAND, OUTPUT);
+  PinMode(SPEAKER, OUTPUT);
+  PinMode(LED, OUTPUT);
+  PinMode(BOARD, INPUT);
+  operation.playing = false;
+}
+
+/*
+ * Main loop to do stuff
+ */
+void loop() {
+  // if (operation.playing) {
+  //     if (digitalRead(BOARD)) { // ie smack side of person viscously
+  //       digitalWrite(LED, HIGH);
+  //       digitalWrite(SPEAKER, HIGH);
+  //       analogWrite()
+  //     }
+  // }
+}
